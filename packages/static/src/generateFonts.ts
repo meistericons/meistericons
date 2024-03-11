f3eb6ee4da5d8311db885e2adf0deba2c9ab24a2
rahulrajdahal <rahulrajdahal@gmail.com>
2024-03-12 03:17:06 +0545

    build: static

---
+++ packages/static/src/generateFonts.ts
@@ -0,0 +1,40 @@
+import { getCurrentDir, readIconFiles, readSvgCode } from '@meistericons/build-tools';
+import { existsSync, mkdirSync, readdirSync, writeFileSync } from 'node:fs';
+import { resolve } from "node:path";
+
+console.log(`Generating fonts...`)
+
+const currentDir = getCurrentDir(import.meta.url);
+const iconsDir = resolve(currentDir, '../../icons');
+
+let totalIcons = 0
+
+readdirSync(iconsDir).forEach((category: string) => {
+    if (category !== 'index.ts') {
+
+
+        const categoryDir = resolve(iconsDir, category);
+        const iconCategories = readIconFiles(categoryDir);
+
+        iconCategories.forEach(async (iconFile) => {
+
+            const svgFile = resolve(iconsDir, `${category}/${iconFile}.svg`);
+            const svgCode = await readSvgCode(svgFile);
+            const outputDir = resolve(currentDir, `../../fonts`);
+            const fontIconsDir = resolve(currentDir, '../../fontIcons')
+
+            if (!existsSync(outputDir)) {
+                mkdirSync(outputDir)
+            }
+            if (!existsSync(fontIconsDir)) {
+                mkdirSync(fontIconsDir)
+            }
+
+            writeFileSync(resolve(fontIconsDir, `${iconFile}.svg`), svgCode, 'utf-8')
+        })
+    }
+    console.log(`Generated icon fonts for - ${category}`)
+    totalIcons++
+})
+
+console.log(`Generated ${totalIcons} icon fonts.`)
