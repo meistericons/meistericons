b1690cdd9df43b9223bd14e90158c90331a27eb2
meistericons <meistericons@gmail.com>
2024-03-23 03:39:34 +0545

    fix: optimize svgs

---
+++ packages/static/src/optimizeIcons.js
@@ -0,0 +1,60 @@
+import { readdirSync, writeFileSync } from "node:fs";
+import { resolve } from "node:path";
+import { optimize } from "svgo";
+import {parseSync,stringify} from 'svgson'
+import { getCurrentDir, readIconFiles, readSvgCode } from '@meistericons/build-tools';
+
+
+const currentDir = getCurrentDir(import.meta.url);
+const iconsDir = resolve(currentDir, "../../icons");
+
+let totalIcons = 0
+
+readdirSync(iconsDir).forEach((category) => {
+    const categoryDir = resolve(iconsDir, category);
+    const iconCategories = readIconFiles(categoryDir);
+
+    iconCategories.forEach(async (iconFile) => {
+        const svgFile = resolve(iconsDir, `${category}/${iconFile}.svg`);
+
+        const svgCode = readSvgCode(svgFile);
+
+        const parsedSvg = parseSync(svgCode);
+
+        parsedSvg.children.forEach((child) => {
+        if (child.attributes.fill === "#000") {
+          child.attributes.fill = "currentColor";
+        }
+      });
+
+
+        const optimizedResult = optimize(stringify(parsedSvg), {
+            multipass: true,
+            js2svg: {
+                indent: 2,
+                pretty: true,
+            },
+            plugins: [
+                { name: "preset-default", params: { overrides: { removeViewBox: false } } },
+                "removeDimensions",
+                "sortAttrs",
+                "prefixIds",
+                {
+                    name: "removeAttrs",
+                    params: {
+                        attrs: ["fill-rule", "clip-rule"],
+                        elemSeparator: ":",
+                        preserveCurrentColor: false,
+                    },
+                },
+            ],
+        });
+        writeFileSync(resolve(iconsDir, `${category}/${iconFile}.svg`), optimizedResult.data, 'utf-8')
+    })
+
+
+
+    console.log(`Optimized ${iconCategories.length} icons for ${category} Category`)
+    totalIcons += iconCategories.length
+})
+console.log(`Optimized ${totalIcons} icons.`)
